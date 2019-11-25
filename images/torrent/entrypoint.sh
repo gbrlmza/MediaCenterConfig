86700b46c0374963ace07ba6ae01a50783ab2dc3
Gabriel Gonzalez <gabriel.gonzalez@mercadolibre.com>
2019-11-25 16:31:30 -0300

    Replace deluge with transmission

--- images/torrent/entrypoint.sh
+++ images/torrent/entrypoint.sh
@@ -3,7 +3,7 @@
 # Start Transmission Daemon
 # -T = No Auth
 # -g = Config Dir
-#transmission-daemon -T -g /transmission 
+transmission-daemon -T -g /transmission
 
 # Flexget Daemon
 #/usr/local/bin/flexget -c /flexget/config.yml daemon start -d --autoreload-config
