c6e82025d01b15c3a927c2325bc46dac35822dbe
Gabriel Gonzalez <gabriel.gonzalez@mercadolibre.com>
2019-11-24 23:18:01 -0300

    Configuration

--- images/deluge/entrypoint.sh
+++ images/deluge/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Start Deluge Daemon & Web-UI
-/usr/bin/deluged -c /config
-/usr/bin/deluge-web -c /config
+/usr/bin/deluged -c /deluge
+/usr/bin/deluge-web -c /deluge
 
 # Flexget Daemon
 /usr/local/bin/flexget -c /flexget/config.yml daemon start -d --autoreload-config
