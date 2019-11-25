7575d87b4c3228274709854fa5dba7408ac8cfde
Gabriel Gonzalez <gabriel.gonzalez@mercadolibre.com>
2019-11-25 15:21:29 -0300

    Transmission

---
+++ images/torrent/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Start Transmission Daemon
+# -T = No Auth
+# -g = Config Dir
+#transmission-daemon -T -g /transmission 
+
+# Flexget Daemon
+#/usr/local/bin/flexget -c /flexget/config.yml daemon start -d --autoreload-config
+
+# Keep it running
+tail -f /dev/null
